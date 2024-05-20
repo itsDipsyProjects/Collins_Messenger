c429c5648e37c468aae4d8212fdfe778b3b00f01
itsDipsyProjects <elliot.collins.git@gmail.com>
2024-05-20 17:02:12 +0200

    Creating the main architecture for the application
    I have started to create a file structure for the serverside code for example registration and login authentication and also init the clientside structure

--- clientside/client_websocket.js
+++ clientside/client_websocket.js
@@ -1,16 +1,10 @@
-const socket = new WebSocket('ws://localhost:8080');
-
-socket.addEventListener('open', function (event) {
-    console.log('Connected to WebSocket server');
-});
-
-socket.addEventListener('message', function (event) {
-    console.log('Received message from server:', event.data);
-});
-
-document.querySelector("button").addEventListener("click", (event) => {
-    // Send data to server
-    socket.send('Hello, boii!');
+export function initWebsocketConnection(){
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.addEventListener('open', function (event) {
+        console.log('Connected to WebSocket server');
+    });
     
-})
-
+    socket.addEventListener('message', function (event) {
+        console.log('Received message from server:', event.data);
+    });
+}
